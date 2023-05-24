621af60db7a2fb0e837c1b3a9b5a03188a24f73b
Shawn Lapuz <lapuzshawn@gmail.com>
2023-05-24 11:31:30 -0700

    Init mini 21 MERN

---
+++ Mini-Project-12-SQL/Develop/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: '',
+    database: 'movies_db'
+  },
+  console.log(`Connected to the movies_db database.`)
+);
+
+// Create a movie
+
+
+// Read all movies
+
+
+// Delete a movie
+
+
+// Read list of all reviews and associated movie name using LEFT JOIN
+
+
+// BONUS: Update review name
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
